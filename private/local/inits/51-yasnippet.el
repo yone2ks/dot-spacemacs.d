2a32b785ec2ec200422b563b153c1800fe94b879
Tsukasa Yoneda <t.yoneda.pub@gmail.com>
2016-02-12 08:15:38 +0900

    update spacemacs 105.9

---
+++ private/local/inits/51-yasnippet.el
@@ -0,0 +1,4 @@
+(require 'yasnippet)
+(setq helm-c-yas-space-match-any-greedy t) ;[default: nil]
+;; (bind-key  "C-i" 'helm-yas-complete yas-minor-mode-map)
+
---
+++ private/local/inits/52-web-mode.el
@@ -0,0 +1,15 @@
+(setq web-mode-ac-sources-alist
+      '(("php" . (ac-source-yasnippet ac-source-php-auto-yasnippets))
+        ("html" . (ac-source-emmet-html-aliases ac-source-emmet-html-snippets))
+        ("css" . (ac-source-css-property ac-source-emmet-css-snippets))))
+
+(add-hook 'web-mode-before-auto-complete-hooks
+          '(lambda ()
+             (let ((web-mode-cur-language
+                    (web-mode-language-at-pos)))
+               (if (string= web-mode-cur-language "php")
+                   (yas-activate-extra-mode 'php-mode)
+                 (yas-deactivate-extra-mode 'php-mode))
+               (if (string= web-mode-cur-language "css")
+                   (setq emmet-use-css-transform t)
+                 (setq emmet-use-css-transform nil)))))
